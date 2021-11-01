d404ed2188f677485dcdc717006138bbc6af67a9
Andy Smith <andy151993@hotmail.com>
2021-11-01 15:55:01 +0000

    live tracks location at all times, marker moves when location changes

--- components/Map.jsx
+++ components/Map.jsx
@@ -3,58 +3,79 @@ import { View, StyleSheet, Dimensions, Text } from "react-native";
 import MapView, { Marker, Polyline } from "react-native-maps";
 import React, { useState, useEffect } from "react";
 import * as Location from "expo-location";
+import { mapStyle } from "../utils/map-style";
 
 const Map = () => {
-    const [location, setLocation] = useState(null);
-    const [errorMsg, setErrorMsg] = useState(null);
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
-    useEffect(() => {
-        (async () => {
-            let { status } = await Location.requestForegroundPermissionsAsync();
-            if (status !== "granted") {
-                setErrorMsg("Permission to access location was denied");
-                return;
-            }
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
 
-            let lastKnownLocation = await Location.getLastKnownPositionAsync(
-                {}
-            );
-            setLocation({
-                longitude: lastKnownLocation.coords.longitude,
-                latitude: lastKnownLocation.coords.latitude,
-                longitudeDelta: 0.0922,
-                latitudeDelta: 0.0421,
-            });
+      try {
+        let lastKnownLocation = await Location.getLastKnownPositionAsync({});
+        setLocation({
+          longitude: lastKnownLocation.coords.longitude,
+          latitude: lastKnownLocation.coords.latitude,
+          longitudeDelta: 0.0922,
+          latitudeDelta: 0.0421,
+        });
+      } catch (err) {
+        console.log(err);
+      }
 
-            let position = await Location.getCurrentPositionAsync({
-                accuracy: Location.Accuracy.BestForNavigation,
-            });
+      let position = await Location.getCurrentPositionAsync({
+        accuracy: Location.Accuracy.BestForNavigation,
+      });
 
-            setLocation({
-                longitude: position.coords.longitude,
-                latitude: position.coords.latitude,
-                longitudeDelta: 0.0921,
-                latitudeDelta: 0.0421,
-            });
-        })();
-    }, []);
+      setLocation({
+        longitude: position.coords.longitude,
+        latitude: position.coords.latitude,
+        longitudeDelta: 0.01,
+        latitudeDelta: 0.01,
+      });
+    })();
+  }, []);
 
-    return (
-        <View>
-            <MapView
-                style={styles.map}
-                initialRegion={location}
-                provider={MapView.PROVIDER_GOOGLE}
-            ></MapView>
-        </View>
-    );
+  setTimeout(async () => {
+    let newPosition = await Location.getCurrentPositionAsync({
+      accuracy: Location.Accuracy.BestForNavigation,
+    });
+
+    setLocation({
+      longitude: newPosition.coords.longitude,
+      latitude: newPosition.coords.latitude,
+      longitudeDelta: 0.0921,
+      latitudeDelta: 0.0421,
+    });
+  }, 10000);
+
+  return (
+    <View>
+      {location && (
+        <MapView
+          style={styles.map}
+          customMapStyle={mapStyle}
+          initialRegion={location}
+          provider={MapView.PROVIDER_GOOGLE}
+        >
+          <Marker coordinate={location} />
+        </MapView>
+      )}
+    </View>
+  );
 };
 
 export default Map;
 
 const styles = StyleSheet.create({
-    map: {
-        width: Dimensions.get("window").width,
-        height: Dimensions.get("window").height,
-    },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
 });
