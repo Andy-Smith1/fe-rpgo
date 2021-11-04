6fe098e06b5d362f14de6c05d394fef88cd141f6
Andy Smith <andy151993@hotmail.com>
2021-11-04 18:12:00 +0000

    force re render when navigating to screen

--- components/Challenges.jsx
+++ components/Challenges.jsx
@@ -2,25 +2,27 @@ import {
   View,
   Text,
   StyleSheet,
-  Button,
   FlatList,
   TouchableOpacity,
 } from "react-native";
 import React, { useState, useContext, useEffect } from "react";
 import { UserContext } from "../contexts/UserContext";
 import { getChallenges } from "../utils/api";
+import { useIsFocused } from "@react-navigation/native";
 
 const Challenges = ({ navigation }) => {
-  const [challenges, setChallenges] = useState(testChallenges);
+  //   const [challenges, setChallenges] = useState(testChallenges);
   const [selectedChallenge, setSelectedChallenge] = useState(null);
   const [apiChallenges, setApiChallenges] = useState([]);
+  const isFocused = useIsFocused();
   const { user } = useContext(UserContext);
 
   useEffect(() => {
     getChallenges().then((challengesFromApi) => {
       setApiChallenges(challengesFromApi);
     });
-  }, [navigation]);
+    console.log("refreshed");
+  }, [isFocused]);
 
   return (
     <View style={styles.container}>
@@ -80,72 +82,72 @@ const styles = StyleSheet.create({
   },
 });
 
-const testChallenges = [
-  {
-    id: 1,
-    title: "Run From the Dragon",
-    description: "You are being chased by a dragon, cover 1000 steps to escape",
-    reward: "blueTrophy.png",
-    activity_type: "stepCount",
-    timed_challenge: { timeLimit: 600000 },
-    activity_value: 10,
-  },
-  {
-    id: 2,
-    title: "Climb the Tower",
-    description:
-      "You find a deserted guard tower, gain 50 elevation to search it",
-    reward: "redTrophy.png",
-    activity_type: "metersClimbed",
-    timed_challenge: null,
-    activity_value: 50,
-  },
-  {
-    id: 3,
-    title: "One does not simply...",
-    description: "Cover 5km to enter a new area",
-    reward: "theOneTrophy.png",
-    activity_type: "distanceTravelled",
-    timed_challenge: null,
-    activity_value: 1,
-  },
-  {
-    id: 4,
-    title: "Help the Grey Mage",
-    description: "Explore an area of 2km to find plants for a mage",
-    reward: "greyTrophy.png",
-    activity_type: "distanceTravelled",
-    timed_challenge: null,
-    activity_value: 2000,
-  },
-  {
-    id: 5,
-    title: "Break the curse",
-    description:
-      "you've been cursed, walk a mile in someone else's shoes to break the curse",
-    reward: "purpleMedal.png",
-    activity_type: "distanceTravelled",
-    timed_challenge: null,
-    activity_value: 1000,
-  },
-  {
-    id: 6,
-    title: "Break the curse",
-    description:
-      "you've been cursed, walk a mile in someone else's shoes to break the curse",
-    reward: "purpleMedal.png",
-    activity_type: "distanceTravelled",
-    timed_challenge: null,
-    activity_value: 1000,
-  },
-  {
-    id: 7,
-    title: "Break the curse",
-    description:
-      "you've been cursed, walk a mile in someone else's shoes to break the curse",
-    reward: "purpleMedal.png",
-    activity_type: "distanceTravelled",
-    timed_challenge: null,
-    activity_value: 1000,
-  },
-];
+// const testChallenges = [
+//   {
+//     id: 1,
+//     title: "Run From the Dragon",
+//     description: "You are being chased by a dragon, cover 1000 steps to escape",
+//     reward: "blueTrophy.png",
+//     activity_type: "stepCount",
+//     timed_challenge: { timeLimit: 600000 },
+//     activity_value: 10,
+//   },
+//   {
+//     id: 2,
+//     title: "Climb the Tower",
+//     description:
+//       "You find a deserted guard tower, gain 50 elevation to search it",
+//     reward: "redTrophy.png",
+//     activity_type: "metersClimbed",
+//     timed_challenge: null,
+//     activity_value: 50,
+//   },
+//   {
+//     id: 3,
+//     title: "One does not simply...",
+//     description: "Cover 5km to enter a new area",
+//     reward: "theOneTrophy.png",
+//     activity_type: "distanceTravelled",
+//     timed_challenge: null,
+//     activity_value: 1,
+//   },
+//   {
+//     id: 4,
+//     title: "Help the Grey Mage",
+//     description: "Explore an area of 2km to find plants for a mage",
+//     reward: "greyTrophy.png",
+//     activity_type: "distanceTravelled",
+//     timed_challenge: null,
+//     activity_value: 2000,
+//   },
+//   {
+//     id: 5,
+//     title: "Break the curse",
+//     description:
+//       "you've been cursed, walk a mile in someone else's shoes to break the curse",
+//     reward: "purpleMedal.png",
+//     activity_type: "distanceTravelled",
+//     timed_challenge: null,
+//     activity_value: 1000,
+//   },
+//   {
+//     id: 6,
+//     title: "Break the curse",
+//     description:
+//       "you've been cursed, walk a mile in someone else's shoes to break the curse",
+//     reward: "purpleMedal.png",
+//     activity_type: "distanceTravelled",
+//     timed_challenge: null,
+//     activity_value: 1000,
+//   },
+//   {
+//     id: 7,
+//     title: "Break the curse",
+//     description:
+//       "you've been cursed, walk a mile in someone else's shoes to break the curse",
+//     reward: "purpleMedal.png",
+//     activity_type: "distanceTravelled",
+//     timed_challenge: null,
+//     activity_value: 1000,
+//   },
+// ];
