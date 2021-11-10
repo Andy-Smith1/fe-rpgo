3f278ed1d6efe091537eb647f16e713fa7fe1401
Andy Smith <andy151993@hotmail.com>
2021-11-10 13:43:02 +0000

    add errorAlert util function, pop up message when api call fails

--- utils/error-alert.js
+++ utils/error-alert.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { Alert } from "react-native";
 
-export const errorAlert = () => {
+const errorAlert = () => {
   Alert.alert("Oops!", "Something went wrong, try again.", [
     { text: "OK", style: "cancel" },
   ]);
 };
+
+export default errorAlert;
